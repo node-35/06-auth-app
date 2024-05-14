80ebfa78b3f3eea045145e52c6c3ee972de1ff1b
andres <andres.david.mm@hotmail.com>
2024-05-13 22:47:10 -0500

    first commit

---
+++ src/routes/user.router.js
@@ -0,0 +1,22 @@
+const { getAll, create, getOne, remove, update, login, getMe } = require('../controllers/user.controllers');
+const express = require('express');
+const verifyJWT = require('../utils/verifyJWT');
+
+const userRouter = express.Router();
+
+userRouter.route('/users')
+    .get(verifyJWT, getAll)
+    .post(create);
+
+userRouter.route('/users/login')
+    .post(login);
+
+userRouter.route('/users/me')
+    .get(verifyJWT, getMe);
+
+userRouter.route('/users/:id')
+    .get(verifyJWT, getOne)
+    .delete(verifyJWT, remove)
+    .put(verifyJWT, update);
+
+module.exports = userRouter;
